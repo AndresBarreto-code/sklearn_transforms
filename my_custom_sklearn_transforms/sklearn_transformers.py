92bb5fe6a253132e4b9daf4519c4c6234593d64d
Andrés Felipe Barreto Guio <64596541+AndresBarreto-code@users.noreply.github.com>
2020-08-27 20:57:25 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -10,7 +10,9 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        data = data.drop(labels=self.columns, axis='columns')
+        data['OBJETIVO'] = data['OBJETIVO'].replace(['Aceptado','Sospechoso'],[1,0])
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
+        return data
